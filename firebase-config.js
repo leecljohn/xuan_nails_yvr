c64be6d7151915f5ab28968d2181988060cf47a2
John Lee <leecljohn@gmail.com>
2026-01-29 16:00:50 -0800

    Separate Firebase config and secure API keys

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// This file contains sensitive Firebase configuration
+// Add this file to .gitignore to keep it out of public repositories
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBp7OWmjMNMt0x9OiVg5ySRaC6Z4MhPGSM",
+  authDomain: "xuannailsyvr-c7514.firebaseapp.com",
+  projectId: "xuannailsyvr-c7514",
+  storageBucket: "xuannailsyvr-c7514.firebasestorage.app",
+  messagingSenderId: "811019827636",
+  appId: "1:811019827636:web:5c194e9d3e4fd1b2089ae1",
+  measurementId: "G-0QCKQXZZ20"
+};
